ec221675ebbaea046b121f01290ccb6a3ade1c88
Unknown <klinger-91@web.de>
2018-07-06 09:34:19 +0200

    added bootstrap & translator.component
    added bootstrap css style classes for an easy start with default styles.
    Also added the translator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { TranslatorComponent } from './translator/translator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
